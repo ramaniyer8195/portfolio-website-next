62562d3593a632e5c69f6c5ba65fb563ceb287e9
Raman Iyer <ramaniyer8195@gmail.com>
2024-06-27 23:40:54 +0530

    Added responsive header component

---
+++ components/Nav.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "blog",
+    path: "/blog",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
+const Nav = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex gap-8">
+      {links.map(({ name, path }, index) => (
+        <Link
+          key={index}
+          href={path}
+          className={`${
+            path === pathname && "text-accent border-b-2 border-accent"
+          } capitalize font-medium hover:text-accent transition-all`}
+        >
+          {name}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Nav;
