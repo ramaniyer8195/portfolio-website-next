f024e19099c2f1c1b9ca82f76b8b37b2012bc658
ramaniyer8195 <130081168+ramaniyer8195@users.noreply.github.com>
2024-06-29 12:29:13 +0530

    Added responsive home page component (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,54 @@
-export default function Home() {
-  return <main>home page</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+// components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row justify-between items-center xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I&apos;m <br />{" "}
+              <span className="text-accent">Raman Iyer</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I am a versatile software developer specializing in the MERN
+              stack. With robust experience in MongoDB, Express.js, React, and
+              Node.js, I excel in building efficient and scalable web
+              applications.
+            </p>
+
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download Resume</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover: transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
+  );
+};
+
+export default Home;
