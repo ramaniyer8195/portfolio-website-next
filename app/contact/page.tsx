567b99aa076ab8a9319109da5aacbfd7cc8b4564
Raman Iyer <ramaniyer8195@gmail.com>
2024-07-02 00:35:39 +0530

    Added responsive contact component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import { motion } from "framer-motion";
+import ContactForm from "@/components/contact/ContactForm";
+import ContactInfo from "@/components/contact/ContactInfo";
+
 const Contact = () => {
-  return <div>contact page</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <ContactForm />
+          </div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ContactInfo />
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
