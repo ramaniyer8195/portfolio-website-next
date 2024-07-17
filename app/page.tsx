a0f003200b7a3192ff3205feda7242479bd6b0e2
Raman Iyer <ramaniyer8195@gmail.com>
2024-07-18 02:17:13 +0530

    Linked resume to download resume button

--- app/page.tsx
+++ app/page.tsx
@@ -23,14 +23,16 @@ const Home = () => {
             </p>
 
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
-              >
-                <span>Download Resume</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a href="/assets/resume/resume.pdf" download="">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download Resume</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
