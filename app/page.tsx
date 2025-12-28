bb05c2d1da0cf0b483923ba2b244f18176ae0bfd
Raman Iyer <ramaniyer8195@gmail.com>
2025-12-28 17:58:20 +0530

    fix build issues

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const Home = async () => {
               <span className="text-accent">Raman Iyer</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I'm a full-stack developer who enjoys building practical, scalable web applications using the MERN stack.
+              I&apos;m a full-stack developer who enjoys building practical, scalable web applications using the MERN stack.
             </p>
 
             <div className="flex flex-col xl:flex-row items-center gap-8">
